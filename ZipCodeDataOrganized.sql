aa21b92f9c382a2f83145311587979122f0ac922
Kala Herbert <kala.herbert719@yahoo.com>
2025-08-01 01:22:06 -0400

    Update ZipCodeDataOrganized.sql

--- ZipCodeDataOrganized.sql
+++ ZipCodeDataOrganized.sql
@@ -1,3 +1,8 @@
+# Select just the city and zip code and rating (not the state)
+# For which the tourist rating is a 4 or higher (this will include all ratings of 4)
+# Sort it descending by tourist rating (so the highest rating is the first record) 
+# Put a comma between all fields
+
 CREATE DATABASE my_zip_codes;
 USE my_zip_codes;
 
